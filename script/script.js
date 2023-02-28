c54d4b3fa25a452b5ec7e44f54605efeb02314fd
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2023-02-28 15:42:13 -0300

    Função que gera todos os cards

--- script/script.js
+++ script/script.js
@@ -31,3 +31,26 @@ async function fetchPokemon(url) {
     });
     return Promise.all(promisesAll);
 }
+
+/*Função que pega a promise gerada pelo fetchPokémon, gera um arrays de objetos com dados de cada pokémon e ao final gera todos os cards*/
+async function cardsGenerator() {
+    const pokemonsInfo = await fetchPokemon(apiUrl);
+    const cards = pokemonsInfo.reduce((acumulator, currentValue) => {
+        acumulator += `
+        <li class="card ${currentValue.types[0]}">
+            <div class="circle">
+                <img src="${currentValue.sprites}" alt="">
+            </div>
+            <div class="name-type">
+                <h2>${currentValue.id}. ${currentValue.name}</h2>
+                <p>${currentValue.types.join(" | ")}</p>
+            </div>
+        </li>
+        `;
+        return acumulator;
+    }, "");
+    const pokemonList = document.querySelector("ul.pokemon-list");
+    pokemonList.innerHTML = cards;
+}
+
+cardsGenerator();
