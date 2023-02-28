f57861db85e0c0a84d08ca41e55f4e5049b531f4
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2023-02-28 15:40:26 -0300

    Função pokemonData que pega todos os detalhes de um pokémon e retorna apenas os necessários

--- script/script.js
+++ script/script.js
@@ -1,5 +1,26 @@
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=151";
 
+/* Função que pega todos os detalhes de um pokémon e seleciona apenas os necessários(id, nome, sprites e tipo) e retorna uma promise */
+async function pokemonData(pokemon) {
+    try {
+        const response = await fetch(pokemon.url);
+        const pokemonJson = await response.json();
+        let pokemonTypes = [];
+        for (const pokemonType of pokemonJson.types) {
+            pokemonTypes.push(pokemonType.type.name);
+        }
+        const pokemonData = {
+            sprites: pokemonJson.sprites.front_default,
+            id: pokemonJson.id,
+            name: pokemonJson.name,
+            types: pokemonTypes
+        }
+        return pokemonData;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 /*Função que realiza o fetch para os 151 primeiros pokémons e mapea apenas os principais dados, retornando uma promise*/
 async function fetchPokemon(url) {
     const response = await fetch(url);
