ea21a468d30034707cd628d59eef0d6534dbc0b4
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2023-02-28 20:59:29 -0300

    Adição do evento de clique aos cards e criação de um modal básico com imagem e descrição

--- script/script.js
+++ script/script.js
@@ -32,17 +32,45 @@ async function fetchPokemon(url) {
     return Promise.all(promisesAll);
 }
 
+/**Requisição para detalhes do pokémon (Apenas um teste para ver se funciona ao realizar requisição em cardsGenerator)*/
+async function showDetails(url) {
+    const responseDetails = await fetch(url);
+    const jsonDetails = await responseDetails.json();
+    console.log(jsonDetails);
+
+    /**Renderização do filtro escuro do fundo*/
+    const body = document.querySelector("body");
+    const filter = document.createElement("div");
+    filter.classList.add("filter");
+    body.prepend(filter);
+
+    /**Renderização do modal */
+    const main = document.querySelector("main.container");
+    const modal = document.createElement("section");
+    modal.classList.add("modal");
+    document.querySelector("body").classList.add("hide");
+    main.appendChild(modal);
+    const modalContent = `
+        <h3>${jsonDetails.name}</h3>
+        <span id="close" class="material-symbols-outlined">close</span>
+        <p class="number">Nº ${jsonDetails.id}</p>
+        <img class="official-artwork" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${jsonDetails.id}.png">
+        <p class="description">${jsonDetails.flavor_text_entries[0].flavor_text.replace("\n", " ").replace("\f", " ")}</p>
+    `;
+    modal.innerHTML = modalContent;
+}
+
 /*Função que pega a promise gerada pelo fetchPokémon, gera um arrays de objetos com dados de cada pokémon e ao final gera todos os cards*/
 async function cardsGenerator() {
     const pokemonsInfo = await fetchPokemon(apiUrl);
     const cards = pokemonsInfo.reduce((acumulator, currentValue) => {
         acumulator += `
-        <li class="card ${currentValue.types[0]}">
+        <li id="${currentValue.id}" class="card ${currentValue.types[0]}">
             <div class="circle">
                 <img src="${currentValue.sprites}" alt="">
             </div>
             <div class="name-type">
-                <h2>${currentValue.id}. <span>${currentValue.name}</span></h2>
+                <h2><span class="id">${currentValue.id}</span>. <span>${currentValue.name}</span></h2>
                 <p>${currentValue.types.join(" | ")}</p>
             </div>
         </li>
@@ -51,6 +79,18 @@ async function cardsGenerator() {
     }, "");
     const pokemonList = document.querySelector("ul.pokemon-list");
     pokemonList.innerHTML = cards;
+    
+    //Capturando todos os cards de Pokémon
+    const pokemonCards = document.querySelectorAll("li.card");
+    pokemonCards.forEach(element => {
+        element.addEventListener("click", () => {
+            pokemonCards.forEach(element => element.classList.add("inactive-card"));
+            const pokemonDetailsUrl = `https://pokeapi.co/api/v2/pokemon-species/${element.id}`;
+            showDetails(pokemonDetailsUrl);
+        });
+    });
 }
 
 cardsGenerator();
+/* const resultado = document.querySelectorAll("li.card");
+console.log(resultado[0]); */
